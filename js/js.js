5e545a9ea36a8f8c9fd6f40a39ab54ad7b48a8ff
JebacKoda <55604333+JebacKoda@users.noreply.github.com>
2019-11-29 01:29:57 +0100

    test

--- js/js.js
+++ js/js.js
@@ -1,11 +1,11 @@
 //Add a JQuery click event handler onto our checkbox.
-$('#check').click(function(){
+$('#terms_and_conditions').click(function(){
     //If the checkbox is checked.
     if($(this).is(':checked')){
         //Enable the submit button.
-        $('#tlacitko1').attr("disabled", false);
+        $('#submit_button').attr("disabled", false);
     } else{
         //If it is not checked, disable the button.
-        $('#tlacitko1').attr("disabled", true);
+        $('#submit_button').attr("disabled", true);
     }
 });
