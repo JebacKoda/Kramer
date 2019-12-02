83d574659921131326d3111117dd338bc1e7803c
Lukáš Šrámek <lukassramek8@gmail.com>
2019-12-02 09:41:26 +0100

    Update funkce.php

--- funkce.php
+++ funkce.php
@@ -1,14 +1,14 @@
 <?php
-if(isset( $_POST['name']))
-    $name = $_POST['name'];
-if(isset( $_POST['prijmeni']))
-    $prijmeni = $_POST['prijmeni'];
-if(isset( $_POST['email']))
-    $email = $_POST['email'];
-if(isset( $_POST['message']))
-    $message = $_POST['message'];
-if(isset( $_POST['cislo']))
-    $cislo = $_POST['cislo'];
+if(isset($_POST['name']) && !empty($_POST['name']))
+    $name = htmlspecialchars(trim($_POST['name']));
+if(isset($_POST['prijmeni']) && !empty($_POST['prijmeni']))
+    $prijmeni = htmlspecialchars(trim($_POST['prijmeni']));
+if(isset( $_POST['email']) && !empty($_POST['email']))
+    $email = htmlspecialchars(trim($_POST['email']));
+if(isset( $_POST['message']) && !empty($_POST['message']))
+    $message = htmlspecialchars(trim($_POST['message']));
+if(isset( $_POST['cislo']) && !empty($_POST['cislo']))
+    $cislo = htmlspecialchars(trim($_POST['cislo']));
 
 $subject = "Email z kramer-nakladace";
 
