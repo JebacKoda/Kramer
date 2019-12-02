603ff6b15dd32b1f6c9995fd4ebebd94a7f7ea63
Lukáš Šrámek <lukassramek8@gmail.com>
2019-12-02 13:20:06 +0100

    Update funkce.php

--- funkce.php
+++ funkce.php
@@ -32,7 +32,7 @@ $subject = "Email z kramer-nakladace";
 $headers = []
 $headers[] = "MIME-Version: 1.0" . "\r\n";
 $headers[] = "Content-type:text/html;charset=UTF-8" . "\r\n";
-$headers[] = 'Content-Transfer-Encoding: 8-bit';
+$headers[] = "Content-Transfer-Encoding: 8-bit";
 $content = "Od: " . $email['name'] . $email['prijmeni'] . "\r\n Číslo: " . $email['cislo'] . "\r\n Email: " . $email['email'] . "\r\n Zpráva: " . $email['message'];
 $recipient = "info@kramer-nakladace.cz";
 
